a03294fc70b045568a7bfcdb831a853ab3f083dc
OSIELJ <osieljuniormartins@gmail.com>
2024-06-08 11:10:09 -0300

    Criando Lógica para navegar entre telas e fazendo a logica de create com o Realtime firebase na activity_upload.xml, teve também a crição da DataClass DadosMoto.kt para manipular os dados .

---
+++ crudrealtimeadmin/src/main/java/com/example/crudrealtimeadmin/DadosMoto.kt
@@ -0,0 +1,10 @@
+package com.example.crudrealtimeadmin
+
+data class DadosMoto(
+    val nomeDono: String? = null,
+    val marcaMoto: String? = null,
+    val placaMoto: String? = null,
+    val modeloMoto: String? = null){
+
+
+}
--- crudrealtimeadmin/src/main/java/com/example/crudrealtimeadmin/UploadActivity.kt
+++ crudrealtimeadmin/src/main/java/com/example/crudrealtimeadmin/UploadActivity.kt
@@ -1,20 +1,60 @@
 package com.example.crudrealtimeadmin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.crudrealtimeadmin.databinding.ActivityMainBinding
+import com.example.crudrealtimeadmin.databinding.ActivityUploadBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class UploadActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityUploadBinding
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_upload)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnUpdateButton.setOnClickListener {
+            val donoMoto = binding.edtDonoMoto.text.toString()
+            val marcaMoto = binding.edtMarcaMoto.text.toString()
+            val placaMoto = binding.edtPlacaMoto.text.toString()
+            val modeloMoto = binding.edtModeloMoto.text.toString()
+
+            databaseReference = FirebaseDatabase.getInstance().getReference("Informção das motos")
+            val dadosMoto = DadosMoto(donoMoto, marcaMoto, placaMoto, modeloMoto)
+            databaseReference.child(placaMoto).setValue(dadosMoto).addOnSuccessListener {
+
+                binding.edtDonoMoto.text.clear()
+                binding.edtMarcaMoto.text.clear()
+                binding.edtPlacaMoto.text.clear()
+                binding.edtModeloMoto.text.clear()
+
+                Toast.makeText(this, "Salvo com sucesso", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@UploadActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+
+                Toast.makeText(this, "Falha ao salvar", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+        }
+
     }
 }
